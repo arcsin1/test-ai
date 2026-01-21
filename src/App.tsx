37e530272d99d0d8a0f39566863c933a81e35137
arcsin1 <zy19931129@gmail.com>
2026-01-21 13:04:15 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
-  console.log(name，fo)
+  console.log(name，fo2)
   return (
     <>
       <div>
