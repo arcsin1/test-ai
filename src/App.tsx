0952bfcbed29881d50b6afe14b2f56f31ebb586c
arcsin1 <zy19931129@gmail.com>
2026-01-21 14:50:39 +0800

    Optimize handleCount with useCallback
    Refactor handleCount to use useCallback for optimization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState,useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const handleCount = () => {
+  const handleCount = useCallback() => {
     setCount((count) => count + 1)
-  }
+  })
   return (
     <>
       <div>
