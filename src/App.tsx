fbed8848aa2e4ec06d395ff498fc7498f7aed618
arcsin1 <zy19931129@gmail.com>
2026-01-23 08:39:10 +0800

    Update handleCount to increment count by 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const handleCount = useCallback() => {
-    setCount((count) => count + 1)
+    setCount((count) => count +  2)
   },[])
   return (
     <>
