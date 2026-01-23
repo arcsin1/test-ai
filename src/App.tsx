97ddb0b49ca9bf43cdcac48f871150b4ce9f391a
arcsin1 <zy19931129@gmail.com>
2026-01-23 14:50:53 +0800

    Fix image tag formatting in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo"  alt="vite logo" />
+          <img src={viteLogo} className="logo" alt="vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
