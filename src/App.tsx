2c2f673d122d479b8451008b5bbfd52a2695cf50
arcsin1 <zy19931129@gmail.com>
2026-01-25 22:41:04 +0800

    Remove logo links from App component
    Removed logo links from the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,6 @@ function App() {
   },[])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="react logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleCount}>
