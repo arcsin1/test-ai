3de3a68db01c6884276c18b8445bb7105bdce885
arcsin1 <zy19931129@gmail.com>
2026-01-27 23:03:36 +0800

    Wrap main content in a div element

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ const  App = () => {
   const handleCount = useCallback() => {
     setCount((count) => count +  1)
   },[])
-
   return (
-    <>
+    <div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleCount}>
@@ -21,7 +20,7 @@ const  App = () => {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
