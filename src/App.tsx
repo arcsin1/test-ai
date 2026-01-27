0ac941fec8fe51250b6f5acbff46cefc34f6b06e
arcsin1 <zy19931129@gmail.com>
2026-01-27 23:09:56 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const  App = () => {
     setCount((count) => count +  1)
   },[])
   return (
-    <div>
+    <>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={handleCount}>
@@ -20,7 +20,7 @@ const  App = () => {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+    </>
   )
 }
 
